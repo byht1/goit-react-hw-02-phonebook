ba416e1c9b670ef3ca546b1cc8ad9a369fccb8d6
byht1 <vitalikbyht1@gmail.com>
2022-07-01 23:20:33 +0300

    hw 2

---
+++ src/components/Phonebook/Filter/Filter.styled.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Input = styled.input`
+  width: 100%;
+  display: block;
+  margin-top: 10px;
+  padding: 5px 10px;
+  font-size: 1em;
+`;
+
+export const Span = styled.span`
+  margin-top: 20px;
+  display: block;
+`;
